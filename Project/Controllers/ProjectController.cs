9971c867de886be9dc876c24d569dffaad083399
abbas <a.abdi97@yahoo.com>
2023-05-05 00:48:09 +0430

    add postjob view/ add resume view / a project detail view / add post resume view

---
+++ Project/Controllers/ProjectController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+	public class ProjectController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult ProjectDetail()
+		{
+			return View();
+		}
+		public IActionResult Resume()
+		{
+			return View();
+		}
+		[HttpGet]
+		public IActionResult PostResume() 
+		{
+			return View();
+		}
+		[HttpGet]
+		public IActionResult PostJob() 
+		{
+			return View();
+		}
+
+	}
+}
